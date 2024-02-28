d120185ac5eee669829250b5901d5db4469fe36d
lunarvibes45 <tealdeal@tutanota.com>
2024-02-28 13:34:01 -0800

    added react-leaflet, leaflet to package.json

--- hospital-finder-map/src/App.js
+++ hospital-finder-map/src/App.js
@@ -10,8 +10,10 @@ import { MapContainer} from 'react-leaflet'
 
 export default function App() {
     return(
+    
       <MapContainer id="map" center={[45.4, -75.7]} zoom={12}scrollWheelZoom={false}>
       </MapContainer>
+    
     
     )
 
