41e2f0ef4d1de4be00aea82cc098722f3b6d7fcf
lunarvibes45 <tealdeal@tutanota.com>
2024-02-27 17:34:00 -0800

    initial commit

---
+++ hospital-finder-map/src/App.js
@@ -0,0 +1,24 @@
+// import SearchResults from './SearchResults';
+// import Search from './Search';
+import './App.css';
+import { MapContainer} from 'react-leaflet'
+
+
+
+
+
+
+export default function App() {
+    return(
+      <MapContainer id="map" center={[45.4, -75.7]} zoom={12}scrollWheelZoom={false}>
+      </MapContainer>
+    
+    )
+
+
+
+  
+  
+}
+
+
---
+++ hospital-finder-map/src/Search.js
@@ -0,0 +1,12 @@
+
+export default function Search(){
+
+    return(
+        <div>
+            <h1>Search Input Box and Search Button</h1>
+            <input value="city,state"></input>
+            <button>Search</button>
+
+        </div>
+    )
+}
